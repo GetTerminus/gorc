142e94116946ec2cfd96bf9dcd2fcce57429d0f5
Brendan Erwin <brendanjerwin@gmail.com>
2019-09-13 20:16:13 -0400

    Use atomic/sync instead of manual mutex

--- gorc.go
+++ gorc.go
@@ -1,78 +1,58 @@
 package gorc
 
 import (
-	"sync"
+	"sync/atomic"
 	"time"
 )
 
+// Gorc is used to keep track of goroutines running.
 type Gorc struct {
-	count      int
+	count      *int32
 	waitMillis time.Duration
-	sync.Mutex
 }
 
 // Inc increases the counter by one.
 func (g *Gorc) Inc() {
-	g.Lock()
-	defer g.Unlock()
-
-	g.count++
+	atomic.AddInt32(g.count, 1)
 }
 
 // IncBy increases the counter by b.
 func (g *Gorc) IncBy(b int) {
-	g.Lock()
-	defer g.Unlock()
-
-	g.count += b
+	atomic.AddInt32(g.count, int32(b))
 }
 
 // Dec decreases the counter by one.
 func (g *Gorc) Dec() {
-	g.Lock()
-	defer g.Unlock()
-
-	g.count--
+	atomic.AddInt32(g.count, -1)
 }
 
 // DecBy decreases the counter by b.
 func (g *Gorc) DecBy(b int) {
-	g.Lock()
-	defer g.Unlock()
-	
-	g.count -= b
+	atomic.AddInt32(g.count, int32(b)*-1)
 }
 
 // GetCount returns an integer holding the count.
-func (g *Gorc) Get() int {
-	g.Lock()
-	defer g.Unlock()
-	
-	return int(g.count)
+func (g *Gorc) GetCount() int {
+	return int(atomic.LoadInt32(g.count))
 }
 
 // SetWaitMillis sets the time in milliseconds the Wait function
 // waits between checking the count against the given integer.
 func (g *Gorc) SetWaitMillis(w int) {
-	g.Lock()
-	defer g.Unlock()
-
 	g.waitMillis = time.Duration(w) * time.Millisecond
 }
 
 // Init initializes a new Gorc instance
 func (g *Gorc) Init() {
-	g.Lock()
-	defer g.Unlock()
-
-	g.count = 0
+	atomic.StoreInt32(g.count, 0)
 	g.waitMillis = 100 * time.Millisecond
 }
 
 // WaitLow will return as soon as the Gorc counter falls below w.
 // e.g. wait until all but w goroutines are stopped.
 func (g *Gorc) WaitLow(w int) {
-	for g.count >= w {
+	l := int32(w)
+	for atomic.LoadInt32(g.count) >= l {
 		time.Sleep(g.waitMillis)
 	}
 	return
@@ -81,7 +61,8 @@ func (g *Gorc) WaitLow(w int) {
 // WaitHigh will return as soon as the Gorc counter goes above w.
 // e.g. wait until at least w goroutines are started.
 func (g *Gorc) WaitHigh(w int) {
-	for g.count <= w {
+	h := int32(w)
+	for atomic.LoadInt32(g.count) <= h {
 		time.Sleep(g.waitMillis)
 	}
 	return
